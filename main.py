4e1f68f2e3c5033dd27b35177746b84331d08532
DiMiRka <fallen_94@bk.ru>
2025-07-25 15:54:44 +1000

    Добавили клас лога

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import argparse
-import sys
-from tabulate import tabulate
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Обработка лог файлов")
+    parser.add_argument("--file", nargs="+", required=True, help="Путь к файлу/файлам")
+    parser.add_argument(
+        "--report", required=True, choices=["average"], help="Тип отчета"
+    )
+    args = parser.parse_args()
