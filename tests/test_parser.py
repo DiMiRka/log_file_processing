da23c56d1100c8135169e3c6f50cc48b60cacbf4
DiMiRka <fallen_94@bk.ru>
2025-07-25 19:42:28 +1000

    поправили тестирование

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,9 @@
 import tempfile
 import pytest
 import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from log_files_parser import parse_log_files
 
 
