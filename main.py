6065ac307ff58bf2bb00976aeb3cda3e7e5d417c
DiMiRka <fallen_94@bk.ru>
2025-07-25 16:30:29 +1000

    фиксация форматером black

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ from exceptions import InvalidReportTypeError
 def main():
     parser = argparse.ArgumentParser(description="Обработка лог файлов")
     parser.add_argument("--file", nargs="+", required=True, help="Путь к файлу/файлам")
-    parser.add_argument("--report", required=True, choices=["average"], help="Тип отчета")
+    parser.add_argument(
+        "--report", required=True, choices=["average"], help="Тип отчета"
+    )
     args = parser.parse_args()
     entries = parse_log_files(args.file)
 
